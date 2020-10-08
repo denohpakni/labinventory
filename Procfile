9916860a9752db9086fe1c5db12fb9e25c4a0265
DMwangi <denohpakni@gmail.com>
2020-10-08 11:24:59 +0300

    added to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn labstockdb.wsgi --log-file -
--- labinventory/settings.py
+++ labinventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qeqoqk2+p1tab-vih53xdqbjkuuylfapuhpnzzy(r41xj25beh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['labstockdb.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,11 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    # ...
-     "labinventory/assets",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ["labinventory/assets",]
 
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_REDIRECT_URL = "/"
