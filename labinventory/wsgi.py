eafcbc35554845136e2163fe890d37f1bdee251c
DMwangi <denohpakni@gmail.com>
2020-10-09 13:50:08 +0300

    -

--- labinventory/wsgi.py
+++ labinventory/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for firstproject project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labinventory.settings')
 
 application = get_wsgi_application()
